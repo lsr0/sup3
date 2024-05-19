a2f3ae8be477c630a6a730df0a40271b988e87cc
Lindsay Roberts <m@lindsayr.com>
2024-05-19 18:47:25 +0300

    .github: restrict integrated tests to ubuntu, use docker over podman

--- test/run-tests-container.sh
+++ test/run-tests-container.sh
@@ -7,7 +7,7 @@ secret_key=SKEXAMPLES3S
 container_runtime=${CONTAINER_RUNTIME:-podman}
 
 echo "sup3-test: running container"
-podman run \
+$container_runtime run \
    -p 9000:9000 \
    -e "MINIO_ROOT_USER=$access_key" \
    -e "MINIO_ROOT_PASSWORD=$secret_key" \
