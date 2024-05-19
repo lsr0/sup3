897e596b142665b244dc32a66a140e27364b426f
Lindsay Roberts <m@lindsayr.com>
2024-05-19 13:33:03 +0300

    test: add integrated test

---
+++ test/run-tests-container.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+access_key=AKEXAMPLES3S
+secret_key=SKEXAMPLES3S
+container_runtime=${CONTAINER_RUNTIME:-podman}
+
+echo "sup3-test: running container"
+podman run \
+   -p 9000:9000 \
+   -e "MINIO_ROOT_USER=$access_key" \
+   -e "MINIO_ROOT_PASSWORD=$secret_key" \
+   quay.io/minio/minio server /data --console-address ":9090" &
+
+function cleanup
+{
+   echo "sup3-test: killing container"
+   kill %%
+}
+trap cleanup EXIT
+
+
+export AWS_ACCESS_KEY_ID=$access_key
+export AWS_SECRET_ACCESS_KEY=$secret_key
+export AWS_ENDPOINT_URL=http://127.0.0.1:9000
+
+test/run-tests.sh $1
---
+++ test/run-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sup3=${1:-sup3}
+
+set -e
+
+d=$(mktemp -d /tmp/bucketXXXX)
+bucket_name=$(basename "$d")
+
+mkdir $d/dir
+echo 1 > $d/dir/a
+echo 1 > $d/dir/bee
+echo 1 > $d/dir/tertiary
+
+bucket=s3://test
+
+$sup3 mb $bucket
+$sup3 cp -r $d/* $bucket
+
+res_unix=$(cd $d && find dir)
+res_sup3=$($sup3 ls -r $bucket)
+
+if [ "$res_unix" != "$res_sup3" ];then
+   echo "Output mismatch:" >&2
+   diff <(echo "$res_unix") <(echo "$res_sup3") --color --unified
+   exit 1
+fi
+echo "sup3-test: success"
