101e906bdb007b8fca55470ac2b22cb7db05a60d
Lindsay Roberts <m@lindsayr.com>
2024-05-25 12:02:04 +0300

    test: add download tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -25,4 +25,20 @@ if [ "$res_unix" != "$res_sup3" ];then
    diff <(echo "$res_unix") <(echo "$res_sup3") --color --unified
    exit 1
 fi
+echo "sup3-test: upload and ls test: success"
+
+down_dir=$(mktemp -d)
+$sup3 cp -r $bucket $down_dir
+echo "sup3-test: $d ~= $down_dir"
+find $d/
+find $down_dir/
+diff -urp $d $down_dir
+
+down_dir2=$(mktemp -d)
+$sup3 cp -r $bucket/dir $down_dir2/
+echo "sup3-test: $d ~= $down_dir2"
+find $d/
+find $down_dir2/
+diff -urp $d $down_dir2
+
 echo "sup3-test: success"
